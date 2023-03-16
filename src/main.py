a5256d8896bc61d92a73a6e13f8bd3a8d453359f
Mark Marryatt <awsumtoast@gmail.com>
2023-03-16 11:41:41 +0200

    slight clean

--- src/main.py
+++ src/main.py
@@ -39,7 +39,9 @@ def get_state():
 
 def get_remaining_pool(state):
     exclusions = state['permanent_exclusions'] + state['already_asked']
-    channel = slack_app.client.conversations_members(channel=settings.CHANNEL_ID)
+    channel = slack_app.client.conversations_members(
+            channel=settings.CHANNEL_ID
+    )
     channel_members = channel['members']
     remaining_pool = [uid for uid in channel_members
                       if uid not in exclusions]
@@ -57,7 +59,8 @@ def ask_for_song():
     remaining_pool, state = get_remaining_pool(state)
     member_id = random.choice(remaining_pool)
     message = generate_message(member_id)
-    slack_app.client.chat_postMessage(channel=CHANNEL_ID, text=message)
+    slack_app.client.chat_postMessage(channel=settings.CHANNEL_ID,
+                                      text=message)
 
     state['already_asked'].append(member_id)
     DOC_REF.set(state)
@@ -66,77 +69,17 @@ def ask_for_song():
 
 @web_app.post("/slack/events", status_code=200)
 async def handle_slack_event(request: Request):
+    # Uncomment this to deal with slack challenge
     # jsonbody = await request.json()
     # return jsonbody.get("challenge")
 
     response = await handler.handle(request)
     return response
 
-    # Parse the incoming Slack event request
-    # body = await request.body()
-    # try:
-    #     req = handler.async_handler.parse_request(body.decode("utf-8"))
-    # except ValueError as e:
-    #     return {"status": 400, "error": str(e)}
-    #
-    # # Handle the app_home_opened event
-    # if req["type"] == "app_home_opened":
-    #     user_id = req["event"]["user"]
-    #     view = {
-    #         "type": "home",
-    #         "blocks": [
-    #             {
-    #                 "type": "section",
-    #                 "text": {
-    #                     "type": "plain_text",
-    #                     "text": f"Welcome to your app home, <@{user_id}>!",
-    #                 },
-    #             },
-    #         ],
-    #     }
-    #     try:
-    #         # Publish a view to the user's app home tab
-    #         print('FURK')
-    #         await slack_app.client.views_publish(user_id=user_id, view=view)
-    #     except SlackApiError as e:
-    #         return {"status": 500, "error": str(e)}
-    #
-    # # Return a 200 response to Slack to acknowledge receipt of the event
-    # return {"status": 200, "message": "Event received"}
-
 
 @slack_app.event("app_mention")
 async def handle_mention(event, say):
     await say("I received your message!")
-    # channel_id = event["channel"]
-    # user_id = event["user"]
-    # text = event["text"]
-    # await slack_app.client.reactions_add(
-    #     channel=channel_id,
-    #     timestamp=event["ts"],
-    #     name="thumbsup"
-    # )
-    # await slack_app.client.chat_postmessage(
-    #     channel=channel_id,
-    #     text=f"hey <@{user_id}>! you said: {text}"
-    # )
-
-
-@slack_app.event("message")
-async def handle_message(event, say):
-    await say("I received your message!")
-    # channel_id = event["channel"]
-    # user_id = event["user"]
-    # text = event["text"]
-    # await slack_app.client.reactions_add(
-    #     channel=channel_id,
-    #     timestamp=event["ts"],
-    #     name="thumbsup"
-    # )
-    # await slack_app.client.chat_postmessage(
-    #     channel=channel_id,
-    #     text=f"hey <@{user_id}>! you said: {text}"
-    # )
 
 
 @slack_app.event("app_home_opened")
