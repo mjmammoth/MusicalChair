69771f369b18a0fe5c6526e91eea0d844ff79993
mjmammoth <awsumtoast@gmail.com>
2023-04-06 16:41:27 +0200

    working firestore, working on meta

--- src/features/playlist_handler.py
+++ src/features/playlist_handler.py
@@ -1,32 +1,24 @@
-import json
 import re
 
 from fastapi import APIRouter
+from google.cloud import firestore
 
 from config import settings
 from app_instances import slack_app
-from storage import bucket, FIRESTORE_CLIENT
+from storage import FIRESTORE_CLIENT
 
 router = APIRouter()
 SONG_COLLECTION = FIRESTORE_CLIENT.collection(settings.SONG_COLLECTION)
 
 
-def update_playlist_blob(url, platform):
-    blob = bucket.blob(f'{platform}_playlist')
-
-    if blob.exists():
-        playlist = blob.download_as_string()
-        playlist = json.loads(playlist)
-    else:
-        playlist = []
-    playlist.append(url)
-    playlist = [*set(playlist)]
-    blob.upload_from_string(json.dumps(playlist))
-
-
 def add_song_to_firestore(song_data):
     # Check if the song already exists in the Firestore collection
-    query = SONG_COLLECTION.where('youtube_url', '==', song_data['youtube_url']).where('spotify_url', '==', song_data['spotify_url']).get()
+    query = (
+                SONG_COLLECTION
+                .where('youtube_url', '==', song_data['youtube_url'])
+                .where('spotify_url', '==', song_data['spotify_url'])
+                .get()
+            )
 
     if query:
         # If the song exists, update the 'posts' array with the new post
@@ -39,7 +31,8 @@ def add_song_to_firestore(song_data):
         SONG_COLLECTION.add(song_data)
 
 
-def handle_song_url(url, event):
+async def handle_song_url(url, event):
+    print(f'Handling song url: {url}')
     song_data = {
         "original_source": "youtube" if "youtube" in url else "spotify",
         "youtube_url": url if "youtube" in url else "",
@@ -65,10 +58,15 @@ async def backfill_playlists():
     result = await slack_app.client.conversations_history(
         channel=settings.CHANNEL_ID)
     messages = result["messages"]
-    urls = []
-    for message in messages:
-        urls.extend(re.findall(r'(https?://\S[^>]+)', message.get('text', '')))
 
-    for url in urls:
-        handle_song_url(url)
+    for message in messages:
+        # Skip messages from the bot
+        bot_id = await slack_app.client.auth_test()
+        if message.get('user') == bot_id['user_id']:
+            continue
+
+        urls = re.findall(r'(https?://\S[^>]+)', message.get('text', ''))
+        for url in urls:
+            await handle_song_url(url, message)
+
     return {'response': 200}
