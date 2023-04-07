d6b40cc1933610a78ca39a73b057fecbaefc993e
mjmammoth <awsumtoast@gmail.com>
2023-04-07 07:42:17 +0200

    interim commit

--- src/features/ask_for_song.py
+++ src/features/ask_for_song.py
@@ -1,5 +1,10 @@
 import random
 from fastapi import APIRouter
+from fastapi import Depends, HTTPException, Header
+from google.oauth2 import id_token
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import Flow
+import os
 
 from config import settings
 from .prompt_questions import generate_prompt
@@ -8,6 +13,44 @@ from app_instances import slack_app
 
 router = APIRouter()
 DOC_REF = FIRESTORE_CLIENT.collection(settings.COLLECTION).document('state')
+
+GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
+GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
+GOOGLE_DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+client_config = {
+    "web": {
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+        "redirect_uris": ["http://localhost:8000/auth_callback"],
+    }
+}
+
+oauth2_flow = Flow.from_client_config(
+    client_config=client_config,
+    scopes=["openid", "email", "profile"],
+    redirect_uri="http://localhost:8000/auth_callback",
+)
+
+
+async def verify_google_token(authorization: str = Header(None)):
+    if not authorization:
+        raise HTTPException(status_code=401, detail="Authorization header is missing")
+
+    try:
+        token = authorization.split(" ")[1]
+        idinfo = id_token.verify_oauth2_token(token, Request(), GOOGLE_CLIENT_ID)
+
+        if idinfo["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
+            raise ValueError("Wrong issuer")
+
+        return idinfo["sub"]
+
+    except ValueError:
+        raise HTTPException(status_code=401, detail="Invalid token")
 
 
 async def get_state():
@@ -40,7 +83,7 @@ async def get_remaining_pool(state):
 
 
 @router.post('/ask-for-song', status_code=200)
-async def ask_for_song():
+async def ask_for_song(user_id: str = Depends(verify_google_token)):
     state = await get_state()
     remaining_pool, state = await get_remaining_pool(state)
     member_id = random.choice(remaining_pool)
