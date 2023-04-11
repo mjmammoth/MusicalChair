2f4b3043066fb603fe530b63efc297f1eda4dbd6
Mark Marryatt <awsumtoast@gmail.com>
2023-04-11 10:48:07 +0200

    refactor + nicer msg

--- src/functions/home_view.py
+++ src/functions/home_view.py
@@ -5,54 +5,41 @@ from state_handler import state
 
 async def get_user_home_block(user_id):
     user_in_pool = await state.is_user_in_pool(user_id)
+
+    # User currently excluded from pool
+    status_text = ":x: You are currently *excluded* from song-of-the-day selections."
+    button_text = ":white_check_mark: Opt-in"
+    button_action = "scheduled_question_opt_in"
+
     if user_in_pool:
         percentage_likely_to_be_asked_next = await state.get_percent_likely_to_be_asked(user_id)
         if percentage_likely_to_be_asked_next == 0:
             percent_message = ":bar_chart: You have already been asked for your song-of-the-day. You will be asked again when everyone else has been asked."
-        elif percentage_likely_to_be_asked_next == 100:
-            percent_message = ":bar_chart: You are guaranteed to be the next person asked for the song-of-the-day! :tada:"
         else:
             percent_message = f":bar_chart: You are {percentage_likely_to_be_asked_next}% likely to be the next person asked."
-        return [
-            {
-                "type": "section",
-                "text": {
-                        "type": "mrkdwn",
-                        "text": f"*Status*:\n\n:white_check_mark: You are currently *included* in the pool to be selected for your song-of-the-day.\n\n{percent_message}"
-                },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": ":x: Opt-out",
-                        "emoji": True
-                    },
-                    "value": "click_me_123",
-                    "action_id": "scheduled_question_opt_out"
-                }
+        status_text = f":white_check_mark: You are currently *included* in the pool to be selected for your song-of-the-day.\n\n{percent_message}"
+        button_text = ":x: Opt-out"
+        button_action = "scheduled_question_opt_out"
 
-            }
-        ]
-    else:
-        return [
-            {
-                "type": "section",
+    return [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"*Status*:\n\n{status_text}"
+            },
+            "accessory": {
+                "type": "button",
                 "text": {
-                    "type": "mrkdwn",
-                    "text": "*Status*:\n\n:x: You are currently *excluded* from song-of-the-day selections."
+                    "type": "plain_text",
+                    "text": button_text,
+                    "emoji": True
                 },
-                "accessory": {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "text": ":white_check_mark: Opt-in",
-                        "emoji": True
-                    },
-                    "value": "click_me_123",
-                    "action_id": "scheduled_question_opt_in"
-                }
+                "value": "click_me_123",
+                "action_id": button_action
             }
-        ]
+        }
+    ]
 
 
 async def home_view_template():
@@ -63,7 +50,14 @@ async def home_view_template():
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": "Welcome!",
+                    "text": "Welcome to MusicalChair!",
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Providing an opportunity for everyone to discover new music and connect over shared interests.\n\nBroaden your horizons and get to know your colleagues better by participating!"
                 }
             },
             {
@@ -91,7 +85,7 @@ footer = [
             {
                     "type": "plain_text",
                     "text": "Author: M J Marryatt",
-                    }
+            }
         ]
     }
 ]
